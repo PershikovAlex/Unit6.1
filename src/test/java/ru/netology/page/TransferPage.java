7282a47002b61d53f9475dee2896cd00b8640baa
Aleksandr Pershikov QA-60 <pershikov-ai@mail.ru>
2023-11-10 23:18:19 +0500

    Udpate TransferPage

--- src/test/java/ru/netology/page/TransferPage.java
+++ src/test/java/ru/netology/page/TransferPage.java
@@ -1,5 +1,18 @@
 package ru.netology.page;
 
-public class TransferPage {
+import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Selenide.$;
+
+public class TransferPage {
+    private SelenideElement firstCardField = $("[data-test-id=92df3f1c-a033-48e6-8390-206f6b1f56c0]");
+    private SelenideElement secondCardField = $("[data-test-id=0f3f5c2a-249e-4c3d-8287-09f7a039391d]");
+    private SelenideElement firstCardButton = $("[data-test-id=92df3f1c-a033-48e6-8390-206f6b1f56c0] .button__text");
+    private SelenideElement secondCardButton = $("[data-test-id=0f3f5c2a-249e-4c3d-8287-09f7a039391d] .button__text");
+    private SelenideElement reloadButton = $("[data-test-id=action-reload] .button__text");
+    public TransferPage() {
+        firstCardButton.click();
+        secondCardButton.click();
+        reloadButton.click();
+    }
 }
