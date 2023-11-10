57b18b362fc3816c16c1a87b9085129acb679e58
Aleksandr Pershikov QA-60 <pershikov-ai@mail.ru>
2023-11-10 19:48:28 +0500

    Update LoginPage.java

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -7,19 +7,13 @@ public class DataHelper {
     }
 
     public static AuthInfo getAuthInfo() {
+
         return new AuthInfo("vasya", "qwerty123");
     }
 
     public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
+
         return new VerificationCode("12345");
-    }
-
-    public static CardInfo getFirstCardNumber() {
-        return new CardInfo("5559 0000 0000 0001");
-    }
-
-    public static CardInfo getSecondCardNumber() {
-        return new CardInfo("5559 0000 0000 0002");
     }
 
     @Value
@@ -31,10 +25,5 @@ public class DataHelper {
     @Value
     public static class VerificationCode {
         String code;
-    }
-
-    @Value
-    public static class CardInfo {
-        String cardNumber;
     }
 }
--- src/test/java/ru/netology/test/TransferMoneyTest.java
+++ src/test/java/ru/netology/test/TransferMoneyTest.java
@@ -1,5 +1,20 @@
 package ru.netology.test;
 
-public class TransferMoneyTest {
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.LoginPage;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TransferMoneyTest {
+    @Test
+    void shouldTest() {
+        Configuration.holdBrowserOpen = true;
+        open("http://localhost:9999");
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        loginPage.validLogin(authInfo);
+    }
 }
