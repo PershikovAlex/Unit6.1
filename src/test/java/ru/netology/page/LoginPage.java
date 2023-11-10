5fb96d33f5c15c36b9f4658a2bef6702176bf8af
Aleksandr Pershikov QA-60 <pershikov-ai@mail.ru>
2023-11-10 21:29:04 +0500

    Add VerificationPage, DashboardPage and update TransferMoneyTest

--- src/test/java/ru/netology/page/LoginPage.java
+++ src/test/java/ru/netology/page/LoginPage.java
@@ -4,16 +4,19 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import ru.netology.data.DataHelper;
 
+import java.util.Calendar;
+
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
     private SelenideElement loginField = $("[data-test-id=login] input");
     private SelenideElement passwordFieled = $("[data-test-id=password] input");
     private SelenideElement loginButton = $("[data-test-id=action-login]");
-    public void validLogin(DataHelper.AuthInfo info) {
+    public VerificationPage validLogin(DataHelper.AuthInfo info) {
         loginField.setValue(info.getLogin());
         passwordFieled.setValue(info.getPassword());
         loginButton.click();
+        return new VerificationPage();
     }
 
 }
--- src/test/java/ru/netology/test/TransferMoneyTest.java
+++ src/test/java/ru/netology/test/TransferMoneyTest.java
@@ -1,6 +1,7 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
 import ru.netology.page.LoginPage;
@@ -9,12 +10,18 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TransferMoneyTest {
-    @Test
-    void shouldTest() {
+    @BeforeEach
+    void setUp() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         var loginPage = new LoginPage();
         var authInfo = DataHelper.getAuthInfo();
-        loginPage.validLogin(authInfo);
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        var dashboardPage = verificationPage.validVerify(verificationCode);
     }
+    @Test
+    void shouldTest() {
+
+        }
 }
